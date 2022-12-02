7096c6066daf3f66babaa60f125be4e289556139
Dalu23 <carrepe24@gmail.com>
2022-12-02 02:37:24 -0400

    Nuevo archivos  con variables

---
+++ index.js
@@ -0,0 +1,13 @@
+let username = 'yorvy'
+let age = 25
+let hasHobbies = false
+let points = [10,20,30]
+let user = {
+      name = 'jhon',
+      lastname = 'sena'
+}
+
+const PI = 3.1415
+
+
+console.log()
